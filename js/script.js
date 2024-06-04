a019aecaa5cc5dc6269b27117649f044987abbc7
TyomhdFadilah <tyomuhamad556@gmail.com>
2024-06-04 17:08:14 +0700

    inisiasi config

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-// Ini Javascript
+function validateForm() {
+    var fullName = document.forms["message-form"]["full-name"].value;
+    var birthDate = document.forms["message-form"]["birth-date"].value;
+     var gender = document.querySelector('input[name="gender"]:checked');
+    var messages = document.forms["message-form"]["messages"].value;
+
+    if (fullName == "" || birthDate == "" || !gender || messages == "") {
+        alert("Mohon lengkapi semua field!");
+        return false;
+    }
+
+    return true;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.forms["message-form"].addEventListener("submit", function(event) {
+        event.preventDefault(); // Prevent default form submission
+
+        // Retrieve form data
+        var fullName = document.forms["message-form"]["full-name"].value;
+        var birthDate = document.forms["message-form"]["birth-date"].value;
+        var gender = document.querySelector('input[name="gender"]:checked').value;
+        var messages = document.forms["message-form"]["messages"].value;
+
+        // Display form data
+        document.getElementById("sender-full-name").textContent = fullName;
+        document.getElementById("sender-birth-date").textContent = birthDate;
+        document.getElementById("sender-gender").textContent = gender;
+        document.getElementById("sender-messages").textContent = messages;
+    });
+});
